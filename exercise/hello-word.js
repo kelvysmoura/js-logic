b3f2be46e60dacc5ef86a757ace1bc13cb0595f4
Kelvys Moura <kelvysmoura2@gmail.com>
2024-04-05 02:36:26 -0300

    Ajustando markdown do desafio do hello world

--- exercise/hello-word.js
+++ exercise/hello-word.js
@@ -2,11 +2,11 @@ export default {
   username: "kelvysmoura",
   title: "Hello world",
   details: `
-    Desafio incial para entender a dinâmica do jogo
+Desafio incial para entender a dinâmica do jogo
 
-    Requisitos
-    - Criar uma função com o nome \`helloWorld\`
-    - A função deve retorna uma string com a frase "Hello World!"
+Requisitos
+- Criar uma função com o nome \`helloWorld\`
+- A função deve retorna uma string com a frase "Hello World!"
   `,
   tests: [
     {
